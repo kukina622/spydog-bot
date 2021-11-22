03607cfd5813a7b82d2f06900dfd04566508532e
min <b10923012@yuntech.edu.tw>
2021-11-22 10:23:47 +0800

    Add cards entity

---
+++ src/entity/cards.ts
@@ -0,0 +1,29 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class cards {
+  @PrimaryGeneratedColumn("increment")
+  cid: number;
+
+  @Column({
+    type: "mediumtext",
+    nullable: true,
+    default: "NULL",
+    collation: "utf8mb4_unicode_ci",
+  })
+  card_name: string;
+
+  @Column({
+    type: "mediumtext",
+    nullable: true,
+    default: "NULL",
+  })
+  card_url: string;
+
+  constructor(param: cards = {} as cards) {
+    const { cid, card_name, card_url } = param;
+    this.cid = cid;
+    this.card_name = card_name;
+    this.card_url = card_url;
+  }
+}
