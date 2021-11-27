3a69887b6e60f8a95312e87bccceff8309cd7442
kukina622 <b10923012@yuntech.edu.tw>
2021-11-27 15:48:22 +0800

    Add userRepo

---
+++ src/repositories/userRepo.ts
@@ -0,0 +1,18 @@
+import { getRepository, Repository } from "typeorm";
+import { users } from "../entity/users";
+
+export class userRepo {
+  private static instance: userRepo;
+  public repo: Repository<users>;
+  private constructor() {
+    this.repo = getRepository(users);
+  }
+  public static init() {
+    if (this.instance === undefined) {
+      this.instance = new userRepo();
+    }
+  }
+  public static getInstance(): userRepo {
+    return this.instance;
+  }
+}
