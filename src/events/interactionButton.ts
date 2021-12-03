f8ecae6d7609b93dcac8e1cda459377253b53100
min <wyes91622@gmail.com>
2021-12-03 20:51:24 +0800

    Disable startGame buuton after clicking

--- src/events/interactionButton.ts
+++ src/events/interactionButton.ts
@@ -1,11 +1,19 @@
-import { Interaction } from "discord.js";
+import { Interaction, MessageActionRow, MessageButton } from "discord.js";
 import { gameService } from "../services";
 
 export async function interactionButtonEvent(interaction: Interaction) {
   if (!interaction.isButton()) return;
   if (interaction.customId === "startgame_confirm") {
     gameService.getInstance().startGame();
-    interaction.reply("開始");
+    // disable button
+    await interaction.update({
+      content:"開始遊戲",
+      components: [
+        new MessageActionRow().addComponents(
+          (interaction.component as MessageButton)?.setDisabled(true)
+        )
+      ]
+    });
   } else if (/^assign_id:\d+$/.test(interaction.customId)) {
     const [assignId] = /\d+/.exec(interaction.customId) as RegExpExecArray;
     const discordId = interaction.user.id;
