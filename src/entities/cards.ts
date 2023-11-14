395077f7e3e756d335e864b2f68332faf8ed743a
Min <wyes91622@gmail.com>
2023-11-14 14:52:52 +0800

    refactor: refactor card type column

--- src/entities/cards.ts
+++ src/entities/cards.ts
@@ -1,6 +1,12 @@
 import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from "typeorm";
 import { assignedCard } from "./assignedCard";
 
+export enum CardType {
+  NORMAL = "normal",
+  SPY = "spy",
+  CUE = "cue"
+}
+
 @Entity()
 export class cards {
   @PrimaryGeneratedColumn("increment")
@@ -24,20 +30,23 @@ export class cards {
   @Column({ default: false })
   hidden_use: boolean;
 
-  @Column({ default: false })
-  is_spycard: boolean;
+  @Column({
+    type: "enum",
+    enum: CardType,
+    default: CardType.NORMAL
+  })
+  type: CardType;
 
   @OneToMany((type) => assignedCard, (Assigned_Card) => Assigned_Card.cards)
   assigned_card: assignedCard[];
 
   constructor(param: cards = {} as cards) {
-    const { cid, card_name, card_url, hidden_use, assigned_card, is_spycard } =
-      param;
+    const { cid, card_name, card_url, hidden_use, assigned_card, type } = param;
     this.cid = cid;
     this.card_name = card_name;
     this.card_url = card_url;
     this.hidden_use = hidden_use;
     this.assigned_card = assigned_card;
-    this.is_spycard = is_spycard;
+    this.type = type;
   }
 }
